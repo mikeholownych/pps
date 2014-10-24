f7cb5ea2fee8765abad47449e3c518c6d837c0fc
Mike Holownych <mike.holownych@gmail.com>
2014-10-24 14:06:54 +0000

    Custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,8 @@ Rails.application.routes.draw do
 
   get '/about' => 'pages#about'
 
+  get '/404' => 'errors#not_found'
+  get '/500' => 'errors#internal_error'
+  get '/422' => 'errors#unprocessable_entity'
+
 end
