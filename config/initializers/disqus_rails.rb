1b88bd2dab165600b03dfc4566e3ed553bdb1269
Mike Holownych <mike.holownych@gmail.com>
2014-11-15 13:16:11 +0000

    Replaced comments with Disqus comments

---
+++ config/initializers/disqus_rails.rb
@@ -0,0 +1,6 @@
+DisqusRails.setup do |config|
+  config::SHORT_NAME = "photoprosites"
+  config::SECRET_KEY = "NC2FfoXf29XETDfg1K4i0JehRImZZMU630ynxbqzfnaBVGyvEtIieBdJC2bgMH5k" #leave blank if not used
+  config::PUBLIC_KEY = "pOiFEXO3AoZkCEtIqBXiweixXqeHV6a2yJ8J6DHcRGacm4DdSRuoJNq8QE98zcta" #leave blank if not used
+  config::ACCESS_TOKEN = "655f1a6ecc4d4a669d38d4a5e760e747" #you got it, right? ;-)
+end
